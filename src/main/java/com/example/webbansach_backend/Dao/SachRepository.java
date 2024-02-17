66a956fc5796ee1d761449765e23b48b369b7b73
thienphu <phutot1111@gmail.com>
2024-02-17 16:12:33 +0700

    update to vd 59

--- src/main/java/com/example/webbansach_backend/Dao/SachRepository.java
+++ src/main/java/com/example/webbansach_backend/Dao/SachRepository.java
@@ -1,9 +1,14 @@
 package com.example.webbansach_backend.Dao;
 
 import com.example.webbansach_backend.Entity.Sach;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "sach")
 public interface SachRepository extends JpaRepository<Sach, Integer> {
+
+    Page<Sach> findByTenSachContainingIgnoreCase(@RequestParam("tenSach") String tenSach, Pageable pageable);
 }
